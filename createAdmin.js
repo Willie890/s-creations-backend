315d8ad0c9e8d186788ec70d2fb82e6ec6e43984
Willie890 <Willemstefanus56@gmail.com>
2025-09-26 18:22:00 +0200

    Update createAdmin.js

--- createAdmin.js
+++ createAdmin.js
@@ -8,8 +8,8 @@ const User = require('./models/User');
 mongoose.connect(process.env.MONGODB_URI);
 
 const createAdmin = async () => {
-  const email = 'admin@screations.com'; // change as needed
-  const password = 'secureAdminPassword123!'; // change this!
+  const email = 'Willemstefanus56@gmail.com'; // change as needed
+  const password = '5678'; // change this!
 
   const hashed = await bcrypt.hash(password, 10);
   const admin = new User({ email, password: hashed, isAdmin: true });
